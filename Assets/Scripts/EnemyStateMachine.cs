61bc9b82171deecf71ad04df09f86e9da64b0fb5
cankulpedin <cankulpedin@gmail.com>
2024-01-22 20:45:55 +0300

    Add hero and enemy state machines

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -1,8 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyStateMachine : MonoBehaviour
 {
     public EnemyData enemy;
+
+    public enum TurnState
+    {
+        PROCCESSING, // time is ticking
+        ADD_TO_LIST, // Add hero to list
+        WAITING, // IDLE
+        SELECTING, // selecting action
+        ACTION, // doing action
+        DEAD
+    }
+
+    public TurnState currentState;
+
+    private float currentCooldown = 0f;
+    private float maxCooldown = 5f;
+
+    void Start()
+    {
+        currentState = TurnState.PROCCESSING;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case TurnState.PROCCESSING:
+                UpdateProgressBar();
+                break;
+            case TurnState.ADD_TO_LIST:
+                break;
+            case TurnState.WAITING:
+                break;
+            case TurnState.SELECTING:
+                break;
+            case TurnState.ACTION:
+                break;
+            case TurnState.DEAD:
+                break;
+        }
+    }
+
+    private void UpdateProgressBar()
+    {
+        currentCooldown = currentCooldown + Time.deltaTime;
+
+        if (currentCooldown > maxCooldown)
+        {
+            currentState = TurnState.ADD_TO_LIST;
+        }
+    }
 }
--- Assets/Scripts/HeroStateMachine.cs
+++ Assets/Scripts/HeroStateMachine.cs
@@ -1,20 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroStateMachine : MonoBehaviour
 {
     public HeroData Hero;
 
-    // Start is called before the first frame update
-    void Start()
+    public enum TurnState
     {
-        
+        PROCCESSING, // time is ticking
+        ADD_TO_LIST, // Add hero to list
+        WAITING, // IDLE
+        SELECTING, // selecting action
+        ACTION, // doing action
+        DEAD
     }
 
-    // Update is called once per frame
+    public TurnState currentState;
+
+    private float currentCooldown = 0f;
+    private float maxCooldown = 5f;
+
+    public Image progressBar;
+
+    void Start()
+    {
+        currentState = TurnState.PROCCESSING;
+    }
+
     void Update()
     {
-        
+        switch (currentState) {
+            case TurnState.PROCCESSING:
+                UpdateProgressBar();
+                break;
+            case TurnState.ADD_TO_LIST:
+                break;
+            case TurnState.WAITING:
+                break;
+            case TurnState.SELECTING:
+                break;
+            case TurnState.ACTION:
+                break;
+            case TurnState.DEAD:
+                break;
+        }
+    }
+
+    private void UpdateProgressBar()
+    {
+        currentCooldown = currentCooldown + Time.deltaTime;
+        float calculatedCooldown = currentCooldown / maxCooldown;
+
+        progressBar.transform.localScale = new Vector3(Mathf.Clamp(calculatedCooldown,0,1),
+            progressBar.transform.localScale.y,
+            progressBar.transform.localScale.z);
+
+        if(currentCooldown > maxCooldown)
+        {
+            currentState = TurnState.ADD_TO_LIST;
+        }
     }
 }
